ba92314e21b37d4714be034dfc5879a94d4668ec
johy <ts04670223@gmail.com>
2020-11-12 15:55:01 +0800

    增加發票寫入資料庫

--- api/add_invoice.php
+++ api/add_invoice.php
@@ -2,5 +2,27 @@
 //撰寫新增消費發票的程式碼
 //將發票的號碼及相關資訊寫入資料庫
 
+$dsn="mysql:host=localhost;dbname=invoice;charset=utf8";
+$pdo=new PDO($dsn,'root','');
+// foreach($_POST as $key => $value){
+//   $tmp[]=$key;
+// }
+// foreach($_POST as $key => $value){
+//   $tmp2[]=$value;
+// }
+// echo "<pre>";
+// print_r($tmp);
+// echo "</pre>";
+echo "<pre>";
+print_r(array_keys($_POST));
+echo "</pre>";
+echo "<br>";
+// echo "inserts into invoices (`".implode("`,`",array_keys($_POST))."`)";
+// echo "values('".implode("','",$_POST)."')";
 
+$sql="insert into invoices (`".implode("`,`",array_keys($_POST))."`)values('".implode("','",$_POST)."')";
+echo $sql;
+$pdo->exec($sql);
+echo "新增完成";
+header("location:../index.php");
 ?>
--- main.php
+++ main.php
@@ -25,31 +25,39 @@
         <a href="" class="btn btn-outline-warning">輸入獎號</a>
       </div>
     </div>
-    <div class="col-8 d-flex justify-content-between p-3 mx-auto border">
+    <div class="col-8 d-flex justify-content-center p-3 mx-auto border">
       <form action="api/add_invoice.php" method="post">
-      <div class="form-group col-md-6">
-        <div>年份:<input class="form-control" type="text" name="year" id=""></div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label>日期:</label>
+            <input class="form-control" type="date" name="date">
+          </div>
+          <div class="form-group col-md-6">
+            <label>期別:</label>
+            <select name="period" id="" class="form-control">
+              <option value="1">1,2</option>
+              <option value="2">3,4</option>
+              <option value="3">5,6</option>
+              <option value="4">7,8</option>
+              <option value="5">9,10</option>
+              <option value="6">11,12</option>
+            </select>
+          </div>
+          <div class="form-group col-md-12">
+            <label>發票號碼:</label>
+            <input class="form-control" type="text" name="code" >
+            <input class="form-control" type="number" name="number">
+          </div>
+          <div class="form-group col-md-12">
+            <label>發票金額:</label>
+            <input class="form-control" type="number" name="payment" >
+          </div>
+          <div class="mx-auto">
+            <input class="btn btn-primary" type="submit" value="送出">
+          </div>
         </div>
-        <div>期別:<select name="" id="">
-            <option value="1">1,2</option>
-            <option value="2">3,4</option>
-            <option value="3">5,6</option>
-            <option value="4">7,8</option>
-            <option value="5">9,10</option>
-            <option value="6">11,12</option>
-          </select>
-          <div>發票號碼:
-            <input type="text" name="prepend" id="">
-            <input type="text" name="number">
-          </div>
-          <div>發票金額:
-            <input type="number" name="payment" id="">
-          </div>
-          <div class="text-center">
-            <input type="submit" value="送出">
-          </div>
-        </div>
-      </form>
     </div>
+    </form>
   </div>
+</div>
 </div>
