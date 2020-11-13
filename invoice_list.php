4151390388fe494ad29e33aefd9d65df845b1deb
johy <ts04670223@gmail.com>
2020-11-13 09:43:54 +0800

    美化畫面及增加要顯示的內容

--- invoice_list.php
+++ invoice_list.php
@@ -1,15 +1,39 @@
 <?php
 include_once("base.php");
 
-$sql="select * from `invoices`";
+$sql = "select * from `invoices` order by date desc";
 
-$rows=$pdo->query($sql)->fetchAll();
+$rows = $pdo->query($sql)->fetchAll();
 
-foreach($rows as $row){
-    echo $row['code'].$row['number']."<br>";
-}
-
-
+// foreach ($rows as $row) {
+//     echo $row['code'] . $row['number'] . "<br>";
+// }
 
 ?>
-
+<table class="table text-center">
+    <thead>
+        <tr>
+            <th>發票號碼</th>
+            <th>消費日期</th>
+            <th>消費金額</th>
+            <th>操作</th>
+        </tr>
+    </thead>
+    <?php
+    foreach ($rows as $row) {
+    ?>
+        <tbody>
+            <tr>
+                <td><?= $row['code'] . $row['number']; ?></td>
+                <td><?= $row['date']; ?></td>
+                <td><?= $row['payment']; ?></td>
+                <td>
+                    <button class="btn btn-dark">編輯</button>
+                    <button class="btn btn-danger">刪除</button>
+                </td>
+            </tr>
+        </tbody>
+    <?php
+    }
+    ?>
+</table>
