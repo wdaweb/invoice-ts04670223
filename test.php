1852c8de86bfa1aa024cfc4f1df61969c3b9b05a
johy <ts04670223@gmail.com>
2020-11-20 11:02:50 +0800

    find()函式撰寫，改為可以代入id或是陣列做為查詢條件

--- test.php
+++ test.php
@@ -2,12 +2,27 @@
 
 include_once "base.php";
 
+// echo implode("&&",['欄位1'=>'值1','欄位2'=>'值2','id'=>'9']);
+// 利用一個暫時陣列存放語句
+$array=['欄位1'=>'值1','欄位2'=>'值2','id'=>'9'];
+foreach($array as $key => $value){
+  $tmp[]=sprintf("`%s`='%s'",$key,$value);
+  // $tmp[]="`".$key."`='".$value."'";
+}
+// print_r($tmp);
+echo implode("&&",$tmp);
+
 function find($table,$id){
   global $pdo;
-  if (is_numeric($id)) {
-    $sql="select * from $table where id='$id'";
+  $sql="select * from $table where ";
+  if (is_array($id)) {
+    foreach($id as $key => $value){
+      $tmp[]=sprintf("`%s`='%s'",$key,$value);
+      // $tmp[]="`".$key."`='".$value."'";
+    }
+    $sql=$sql.implode(" && ",$tmp);
   }else{
-    $sql="select * from $table where $id";
+    $sql=$sql."id='$id'";
   }
 
   $row=$pdo->query($sql)->fetch();
@@ -15,11 +30,14 @@ function find($table,$id){
   return $row;
 }
 
+
 $row=find('invoices',16);
-echo $row['code'].$row['number']."<br>";
+echo "<br>".$row['code'].$row['number']."<br>";
 
 $row=find('invoices',17);
 echo $row['code'].$row['number']."<br>";
 
+$row=find('invoices',['code' => 'FJ','number'=>'61956613']);
+echo $row['code'].$row['number']."<br>";
 
 ?>
